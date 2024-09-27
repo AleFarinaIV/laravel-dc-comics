e2c0147758b635d6fcd5d55f540dbdef7464ef0d
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-27 17:21:19 +0200

    [UPD] setup seeder run() function and added faker data into table

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use Faker\Generator as Faker;
 
 class ComicSeeder extends Seeder
 {
@@ -12,8 +14,25 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $comic = new Comic();
+
+            $comic->title = $faker->sentence(5);
+            $comic->description = $faker->text(200);
+            $comic->thumb = $faker->imageUrl();
+            $comic->price = $faker->randomFloat(2,1,100);
+            $comic->series = implode(' ', $faker->words(10, false));
+            $comic->sale_date = $faker->date();
+            $comic->type = $faker->randomElement([
+                'graphic novel',
+                'comic book',
+                'manga',
+                'webcomic'
+            ]);
+
+            $comic->save();
+        }
     }
 }
